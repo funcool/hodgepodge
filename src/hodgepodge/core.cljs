f2be0465c99c1ec3ace3d69b7daa78653f99559c
Alejandro Gómez <alejandro@dialelo.com>
2014-11-14 00:45:53 +0000

    add iwatchable skel

--- src/hodgepodge/core.cljs
+++ src/hodgepodge/core.cljs
@@ -39,6 +39,17 @@
        (if-let [i (.getItem s (serialize key))]
          (deserialize i)
          not-found)))
+
+  IWatchable
+  (-notify-watches [this oldval newval]
+    ; TODO
+    )
+  (-add-watch [this key f]
+    ; TODO
+    )
+  (-remove-watch [this key]
+    ; TODO
+    )
 )
 
  (comment
