65e63aa59f06554fa83faafc81ca0e16a8bce538
Alejandro Gómez <alejandro@dialelo.com>
2015-01-10 14:14:03 +0100

    Prepare for bugfix release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hodgepodge "0.2.0-SNAPSHOT"
+(defproject hodgepodge "0.1.1"
   :description "A idiomatic ClojureScript interface to HTML5 storage"
   :url "https://github.com/dialelo/hodgepodge"
   :license {:name "BSD (2 Clause)"
