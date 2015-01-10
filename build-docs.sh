98e05ec66fbf47105f6127592ac380065c1e9e5a
Alejandro Gómez <alejandro@dialelo.com>
2015-01-10 14:38:54 +0100

    Update docs

--- build-docs.sh
+++ build-docs.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-(cd docs; make)
-cp -vr docs/index.html /tmp/index.html;
-git checkout gh-pages;
 
+(cd docs && make)
+cp -vr docs/index.html /tmp/index.html
+git checkout gh-pages
+
+cp /tmp/index.html index.html
 git add -u
 git commit -a -m "Update docs"
 git push origin gh-pages
