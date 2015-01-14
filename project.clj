d553da29e7f550dad03c227034e479b424e702bf
Andrey Antukh <niwi@niwi.be>
2015-01-14 18:47:07 +0100

    Fix project.clj
    This is a detailed list of changes:
    
    - Moving some development dependencies to :devel profile.
    - Moving hooks to :devel profile.
    - Set to clojure and clojurescript "provided" scope.
    
    Having some of development dependencies as runtime dependencies
    can cause very strange behavior on clojurescript compiler on
    applications that are using this package. One clear example
    of the strange behavior is: compiling part of namespaces (cljs.core)
    with old clojurescript version.

--- project.clj
+++ project.clj
@@ -4,16 +4,24 @@
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :source-paths ["src"]
-  :dependencies [[org.clojure/clojurescript "0.0-2498"]
-                 [org.clojure/clojure "1.6.0"]]
+
+  :dependencies [[org.clojure/clojurescript "0.0-2498" :scope "provided"]
+                 [org.clojure/clojure "1.6.0" :scope "provided"]]
+
   :scm {:name "git"
         :url "https://github.com/dialelo/hodgepodge"}
-  :hooks [leiningen.cljsbuild]
-  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  :jar-exclusions [#"^test/.*"]
+  :clean-targets ^{:protect false} ["target"
+                                    "resources/test/out"
+                                    "resources/test/test.js"]
+
+  :profiles {:dev {:hooks [leiningen.cljsbuild]
+                   :plugins [[lein-cljsbuild "1.0.4"]]}}
   :cljsbuild {:builds {:test
                        {:source-paths ["src" "test"]
                         :compiler {
                             :output-to "resources/test/test.js"
-                            :output-dir "resources/test"
+                            :output-dir "resources/test/out"
                             :optimizations :advanced}}}
               :test-commands {"unit" ["phantomjs" "resources/test/runner.js"]}})
